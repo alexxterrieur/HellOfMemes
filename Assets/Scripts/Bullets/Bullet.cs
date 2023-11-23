fa6f1ba37850524a7bb867b291e11774adb167c9
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2023-11-23 13:41:00 +0100

    player movement + first ennemi

---
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector2 moveDirection;
+    private float moveSpeed;
+
+    private void OnEnable()
+    {
+        Invoke("Destroy", 3f);
+    }
+
+    private void Start()
+    {
+        moveSpeed = 5f;
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    public void SetMoveDirection(Vector2 dir)
+    {
+        moveDirection = dir;
+    }
+
+    public void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision != null)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                Debug.Log("igfuig");
+            }
+        }
+    }
+}
